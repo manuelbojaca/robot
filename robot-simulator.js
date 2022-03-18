cf14ba13c1b98b6951b72046dc565b7267c5ca4b
Sergio <sergio@bellpi.com>
2022-03-18 17:45:17 -0500

    simulator robot

---
+++ robot-simulator.js
@@ -0,0 +1,48 @@
+export class InvalidInputError extends Error {
+  constructor(message) {
+    super();
+    this.message = message;
+  }
+}
+
+export class Robot {
+  orient() {
+    //Delete and write code
+  }
+
+  get bearing() {
+    //Delete and write code
+  }
+
+  get coordinates() {
+    //Delete and write code
+  }
+
+  turnRight() {
+    //Delete and write code
+  }
+
+  turnLeft() {
+    //Delete and write code
+  }
+
+  at() {
+    //Delete and write code
+  }
+
+  advance() {
+    //Delete and write code
+  }
+
+  static instructions() {
+    //Delete and write code
+  }
+
+  place() {
+    //Delete and write code
+  }
+
+  evaluate() {
+    //Delete and write code
+  }
+}
