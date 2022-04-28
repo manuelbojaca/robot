f72b391c2cd2049dadaf42c1e6c750c58b00ebd3
Manuel Bojaca <manuel.bojaca@outlook.com>
2022-04-28 17:27:33 -0500

    Finish all test ok

--- robot-simulator.js
+++ robot-simulator.js
@@ -5,9 +5,10 @@ export class InvalidInputError extends Error {
   }
 }
 
-const directions = ["east", "west", "north", "south"];
+const directions = ["north", "east", "south", "west"];
 
 export class Robot {
+
   orient(position) {
     if (!directions.includes(position)) {
       throw new InvalidInputError("wrong error");
@@ -21,32 +22,112 @@ export class Robot {
   }
 
   get coordinates() {
-    //Delete and write code
+    return [this.x, this.y];
   }
 
   turnRight() {
-    //Delete and write code
+    if (this.location === "north") {
+      this.orient("east");
+      return;
+    }
+    if (this.location === "east") {
+      this.orient("south");
+      return;
+    }
+    if (this.location === "south") {
+      this.orient("west");
+      return;
+    }
+    if (this.location === "west") {
+      this.orient("north");
+      return;
+    }
   }
 
   turnLeft() {
-    //Delete and write code
+    if (this.location === "north") {
+      this.orient("west");
+      return;
+    }
+    if (this.location === "east") {
+      this.orient("north");
+      return;
+    }
+    if (this.location === "south") {
+      this.orient("east");
+      return;
+    }
+    if (this.location === "west") {
+      this.orient("south");
+      return;
+    }
   }
 
-  at() {
-    //Delete and write code
+  at(x, y) {
+    this.x = x;
+    this.y = y;
   }
 
   advance() {
-    //Delete and write code
+    if(this.location === "north") {
+      this.x = this.x;
+      this.y = this.y + 1;
+    }
+    if(this.location === "east") {
+      this.x = this.x + 1;
+      this.y = this.y;
+    }
+    if(this.location === "south") {
+      this.x = this.x;
+      this.y = this.y - 1;
+    }
+    if(this.location === "west") {
+      this.x = this.x - 1;
+      this.y = this.y;
+    }
   }
 
-  static instructions() {
-    //Delete and write code
+  static instructions(instruction) {
+
+    return instruction.split('').map(function (item) {
+        
+        if(item === "L"){
+          return "turnLeft";
+        }
+        if(item === "R"){
+          return "turnRight";
+        }
+        if(item === "A"){
+          return "advance";
+        }
+    });  
+  
   }
-  place() {
-    //Delete and write code
+
+  place(obj) {
+    this.at(obj.x, obj.y);
+    this.location = obj.direction;
   }
-  evaluate() {
-    //Delete and write code
+
+  evaluate(str) {
+    let arr = this.constructor.instructions(str);
+
+    arr.forEach((item) => {
+      console.log('itemi', item);
+      if (item === "turnRight") {
+        this.turnRight();
+        console.log('right');
+      }
+      if (item === "turnLeft") {
+        this.turnLeft();
+        console.log('left');
+      }
+      if (item === "advance") {
+        this.advance();
+        console.log('advance');
+      }
+      console.log('location: ', this.location, ' x: ', this.x, ' y: ', this.y);
+    });  
   }
 }
+console.log(Robot);
